d66ec1232f5a014764f4d2671ca8f84f9447aaff
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2023-09-22 13:48:47 -0700

    Add missing securable collection and map voters

--- gsec/src/test/java/gemma/gsec/ApplicationContextTest.java
+++ gsec/src/test/java/gemma/gsec/ApplicationContextTest.java
@@ -1,13 +1,42 @@
 package gemma.gsec;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.SecurityConfig;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
-@ContextConfiguration(locations = {"classpath*:gemma/gsec/applicationContext-*.xml", "classpath:gemma/gsec/testContext.xml"})
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@ContextConfiguration(locations = { "classpath*:gemma/gsec/applicationContext-*.xml", "classpath:gemma/gsec/testContext.xml" })
 public class ApplicationContextTest extends AbstractJUnit4SpringContextTests {
 
+    @Autowired
+    private AccessDecisionManager accessDecisionManager;
+
+    /**
+     * Test all the supported {@link org.springframework.security.access.annotation.Secured} config attribute.
+     */
     @Test
-    public void test() {
+    public void testSupportedSecuredConfigAttributes() {
+        assertTrue( accessDecisionManager.supports( new SecurityConfig( "ACL_SECURABLE_READ" ) ) );
+        assertTrue( accessDecisionManager.supports( new SecurityConfig( "ACL_SECURABLE_EDIT" ) ) );
+        assertTrue( accessDecisionManager.supports( new SecurityConfig( "ACL_SECURABLE_COLLECTION_READ" ) ) );
+        assertTrue( accessDecisionManager.supports( new SecurityConfig( "ACL_SECURABLE_COLLECTION_EDIT" ) ) );
+        assertTrue( accessDecisionManager.supports( new SecurityConfig( "ACL_SECURABLE_MAP_READ" ) ) );
+        assertTrue( accessDecisionManager.supports( new SecurityConfig( "ACL_SECURABLE_MAP_EDIT" ) ) );
+        assertTrue( accessDecisionManager.supports( new SecurityConfig( "ACL_SECURABLE_MAP_VALUE_READ" ) ) );
+        assertTrue( accessDecisionManager.supports( new SecurityConfig( "ACL_SECURABLE_MAP_VALUE_EDIT" ) ) );
+        assertFalse( accessDecisionManager.supports( new SecurityConfig( "ACL_FOO" ) ) );
+        assertTrue( accessDecisionManager.supports( new SecurityConfig( "GROUP_USER" ) ) );
+        assertTrue( accessDecisionManager.supports( new SecurityConfig( "GROUP_ADMIN" ) ) );
+        assertTrue( accessDecisionManager.supports( new SecurityConfig( "GROUP_AGENT" ) ) );
+        assertTrue( accessDecisionManager.supports( new SecurityConfig( "GROUP_ANONYMOUS" ) ) );
+        assertTrue( accessDecisionManager.supports( new SecurityConfig( "IS_AUTHENTICATED_ANONYMOUSLY" ) ) );
+        assertTrue( accessDecisionManager.supports( new SecurityConfig( "GROUP_RUN_AS_USER" ) ) );
+        assertTrue( accessDecisionManager.supports( new SecurityConfig( "GROUP_RUN_AS_ADMIN" ) ) );
+        assertTrue( accessDecisionManager.supports( new SecurityConfig( "GROUP_RUN_AS_ANONYMOUS" ) ) );
     }
 }
