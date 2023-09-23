c4aa1f7e6334dd96ad1d334d7c88fdd6a57a317a
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2023-09-23 02:05:17 -0700

    Add a test case for reproducing #25

--- gsec/src/test/java/gemma/gsec/acl/afterinvocation/AclEntryAfterInvocationCollectionFilteringProviderTest.java
+++ gsec/src/test/java/gemma/gsec/acl/afterinvocation/AclEntryAfterInvocationCollectionFilteringProviderTest.java
@@ -3,6 +3,7 @@ package gemma.gsec.acl.afterinvocation;
 import gemma.gsec.acl.domain.AclService;
 import gemma.gsec.model.Securable;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.acls.domain.BasePermission;
@@ -13,13 +14,13 @@ import org.springframework.security.acls.model.ObjectIdentity;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.Authentication;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.Mockito.*;
@@ -57,5 +58,38 @@ public class AclEntryAfterInvocationCollectionFilteringProviderTest {
         assertEquals( 3, result.size() );
         verify( acl, times( 2 ) ).isGranted( any(), any(), eq( false ) );
         verifyNoMoreInteractions( acl );
+    }
+
+    /**
+     * Hashing an element triggers a proxy initialization with Hibernate, which is not desirable.
+     */
+    @Test
+    @Ignore("This is an issue in Spring Security where a HashSet is used for removing proxies. See https://github.com/PavlidisLab/gsec/issues/25 for details.")
+    public void testRemovedCollectionElementsAreNotHashed() {
+        Acl acl = mock( Acl.class );
+        when( acl.isGranted( any(), any(), anyBoolean() ) ).thenReturn( false );
+        when( aclService.readAclsById( any() ) )
+            .thenAnswer( a -> a.getArgument( 0, Collection.class ).stream()
+                .distinct()
+                .collect( Collectors.toMap( o -> ( ObjectIdentity ) o, o -> acl ) ) );
+        Securable a = new Securable() {
+            @Override
+            public Long getId() {
+                return 1L;
+            }
+
+            @Override
+            public int hashCode() {
+                throw new UnsupportedOperationException( "I cannot be hashed!" );
+            }
+        };
+        Collection<?> collection = new ArrayList<>( Arrays.asList( a, mock( Securable.class ) ) );
+        Collection<?> result = ( Collection<?> ) voter.decide( auth, null, Collections.singletonList( ca ), collection );
+        assertSame( collection, result );
+        assertTrue( result.isEmpty() );
+        verify( acl, times( 2 ) ).isGranted( any(), any(), eq( false ) );
+        verifyNoMoreInteractions( acl );
+        verify( a ).getId();
+        verifyNoMoreInteractions( a );
     }
 }
